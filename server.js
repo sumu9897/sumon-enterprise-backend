3e8aea053160d9c8d2540681e9496e484866f65c
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2026-02-16 10:39:32 +0600

    update server

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
 require('dotenv').config();
-const app = require('./app');
-const connectDB = require('./config/database');
+const app = require('../src/app');
+const connectDB = require('../src/config/database');
 
-// Handle uncaught exceptions
-process.on('uncaughtException', (err) => {
-  console.error('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.error(err.name, err.message);
-  process.exit(1);
-});
+// Connect DB (serverless safe)
+let isConnected = false;
 
-// Connect to database
-connectDB();
+async function handler(req, res) {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
 
-const PORT = process.env.PORT || 5002;
+  return app(req, res);
+}
 
-const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err) => {
-  console.error('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.error(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-// Graceful shutdown
-process.on('SIGTERM', () => {
-  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-  server.close(() => {
-    console.log('💥 Process terminated!');
-  });
-});
+module.exports = handler;
